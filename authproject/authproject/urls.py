d13e089a67a0eb42bf790d973583bb397f4e1a6f
harshsaxena40601 <146015392+harshsaxena40601@users.noreply.github.com>
2025-04-14 08:18:09 +0530

    h

--- authproject/authproject/urls.py
+++ authproject/authproject/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from accounts.views import home 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('accounts.urls')),
+    path('', home, name='home'),
 ]
